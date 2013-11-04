81c03086a01bad3a204fb2956fd9c4b5e2b003ad
Bruno Simon <HETIC@MacBook-Pro-de-Bruno.local>
2013-11-04 15:35:16 +0100

    Update canvas particules G2 + Avancée cubestagram G2

--- www/cours-9-G2-canvas-anim/script.js
+++ www/cours-9-G2-canvas-anim/script.js
@@ -1,7 +1,10 @@
 var canvas     = null,
     context    = null,
     coords     = null,
-    particules = null;
+    particules = null,
+    count      = 5,
+    gravity    = 0.05,
+    stats      = null;
 
 window.onload = function()
 {
@@ -19,11 +22,29 @@ window.onload = function()
         coords.y = e.clientY;
     };
 
+    stats = new Stats();
+    stats.setMode(1); // 0: fps, 1: ms
+
+    // Align top-left
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+
+    document.body.appendChild( stats.domElement );
+
     requestAnimFrame(loop);
+
+
+    var gui = new dat.GUI();
+    gui.add(window,'count',0,10);
+    gui.add(window,'gravity',-0.5,0.5);
 };
 
 function loop()
 {
+    stats.end();
+    stats.begin();
+
     requestAnimFrame(loop);
     update();
     create();
@@ -52,7 +73,7 @@ function update()
     for(var i = 0; i < particules.length; i++)
     {
         particule = particules[i];
-        particule.speed_y += 0.05;
+        particule.speed_y += gravity;
         particule.x += particule.speed_x;
         particule.y += particule.speed_y;
         particule.life--;
@@ -69,7 +90,7 @@ function create()
 {
     if(particules.length < 10000)
     {
-        for(var i = 0; i < 5; i++)
+        for(var i = 0; i < count; i++)
         {
             var particule = {
                 x       : coords.x,
