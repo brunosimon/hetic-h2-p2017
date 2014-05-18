4fd8c42ab93342197a8eaad17603cded0e36fd9f
Bruno Simon <simon.bruno.77@gmail.com>
2014-05-18 23:58:02 +0200

    Update cours 27

---
+++ exercices/cours-27-G1-php-silex/web/index.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+// Twig
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+// Home
+$app->get('/', function() {
+    return 'Accueil !';
+});
+
+// Page
+$app->get('/page/{page}',function($page)
+{
+    return 'Page '.$page;
+})
+->assert('page','\d+');
+
+// Category
+$app->get('/category/{category}',function($category)
+{
+    return 'Category '.$category;
+})
+->assert('category','[a-z0-9-]+');
+
+$app->run();
--- exercices/cours-27-php-silex/web/index.php
+++ exercices/cours-27-php-silex/web/index.php
@@ -1,8 +1,11 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
+require_once 'config.php';
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
 
 // Twig
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -10,38 +13,82 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 ));
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
+
 // Home
-$app->get('/',function(Silex\Application $app)
+$app->get('/',function()
 {
+    // Globals
+    global $app;
+    global $snippets_model;
+
+
+    // Prepare data
     $data = array(
-        'title' => 'Home'
+        'title'    => 'Home',
+        'snippets' => $snippets_model->get(),
+        'pages'    => $snippets_model->get_pages()
     );
     return $app['twig']->render('home.twig',$data);
 })
 ->bind('home');
 
+
 // Pages
-$app->get('page/{page}',function(Silex\Application $app, $page)
+$app->get('page/{page}',function($page)
 {
+    // Globals
+    global $app;
+    global $snippets_model;
+
+    // Get snippets
+    $snippets = $snippets_model->get_by_page($page);
+
+    // Prepare data
     $data = array(
-        'title' => 'Page '.$page,
-        'page'  => $page
+        'title'    => 'Page '.$page,
+        'snippets' => $snippets,
+        'page'     => $page,
+        'pages'    => $snippets_model->get_pages($page)
     );
     return $app['twig']->render('page.twig',$data);
 })
 ->assert('page','\d+') // Number
 ->bind('page');
 
+
 // Categories
-$app->get('category/{category}',function(Silex\Application $app, $category)
+$app->get('category/{category}',function($category)
 {
+    // Globals
+    global $app;
+    global $snippets_model;
+
+    // Get snippets
+    $snippets = $snippets_model->get_by_category_slug($category);
+
+    // Prepare data
     $data = array(
         'title'    => 'Category : '.$category,
-        'category' => $category
+        'category' => $category,
+        'snippets' => $snippets
     );
     return $app['twig']->render('category.twig',$data);
 })
 ->assert('category','[a-z0-9-]+') // Slug
 ->bind('category');
+
+
+// Errors
+$app->error(function (\Exception $e, $code)
+{
+    // Globals
+    global $app;
+
+    $data = array(
+        'title' => $code
+    );
+    if($code == 404)
+        return $app['twig']->render('404.twig',$data);
+});
 
 $app->run();
